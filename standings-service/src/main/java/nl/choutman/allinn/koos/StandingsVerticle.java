416bcc74c97db0b9fab4d133e842368ddf7986b6
Coen Houtman <coen.houtman@ordina.nl>
2017-05-16 11:04:40 +0200

    Move model and dao classes to a separate core project

--- standings-service/src/main/java/nl/choutman/allinn/koos/StandingsVerticle.java
+++ standings-service/src/main/java/nl/choutman/allinn/koos/StandingsVerticle.java
@@ -3,8 +3,13 @@ package nl.choutman.allinn.koos;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.eventbus.MessageConsumer;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Route;
 import io.vertx.ext.web.Router;
 
@@ -21,11 +26,18 @@ public class StandingsVerticle extends AbstractVerticle {
 
         route.handler(routingContext -> {
             HttpServerResponse response = routingContext.response();
-            response.end();
+            JsonArray standings = new JsonArray();
+            standings.add(new JsonObject("{\"teamName\": \"Drive\"}"));
+            response.end(Json.encode(standings));
         });
 
         HttpServer httpServer = vertx.createHttpServer();
         httpServer.requestHandler(router::accept).listen(8080);
+
+        EventBus eventBus = vertx.eventBus();
+        eventBus.consumer("bla", event -> {
+
+        });
     }
 
     public static void main(String[] args) {
