d85073f434a326339f9099a1669a727d4ae8a0f9
Coen Houtman <coen.houtman@ordina.nl>
2017-05-24 16:54:46 +0200

    Additional match component tests
    Scenarios:
     - Do not display a card when the bounded match is not an object
     - Do not display a card when not all expected values are present
     - Display a card when all expected values are present

--- frontend/app/components/match/match.component.test.js
+++ frontend/app/components/match/match.component.test.js
@@ -24,6 +24,41 @@ describe('Components', function () {
 
             expect(mdCard.hasClass("ng-hide")).toBe(true);
         }));
+
+        it('should not display anything when match is not a JSON object', inject(function () {
+            var element = $compile("<match match='match'></match>")($scope);
+            $scope.match = "not a JSON";
+
+            $scope.$digest();
+
+            var mdCard = element.find('md-card');
+
+            expect(mdCard.hasClass("ng-hide")).toBe(true);
+        }));
+
+        it('should not display anything when match is missing a value in the JSON object', inject(function () {
+            var element = $compile("<match match='match'></match>")($scope);
+            $scope.match = {'homeTeamName': 'team1', 'dateTime': '20170101T12:00:00+0100'};
+
+            $scope.$digest();
+
+            var mdCard = element.find('md-card');
+
+            expect(mdCard.hasClass("ng-hide")).toBe(true);
+        }));
+
+        it('should display a match if all variables are present', inject(function () {
+            var element = $compile("<match match='match'></match>")($scope);
+            $scope.match = {'homeTeamName': 'team1', 'awayTeamName': 'team2', 'dateTime': '20170101T12:00:00+0100'};
+
+            $scope.$digest();
+
+            var mdCardTitleTextElement = element.find('span');
+
+            expect(mdCardTitleTextElement[0].innerHTML).toEqual("team1");
+            expect(mdCardTitleTextElement[1].innerHTML).toContain("VS");
+            expect(mdCardTitleTextElement[2].innerHTML).toEqual("team2");
+        }));
     });
 
 });
